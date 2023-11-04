476721784ca17e4ea37178f2736f030ac6db5427
theopathic <73636682+theopathic@users.noreply.github.com>
2023-11-03 21:18:56 -0600

    Update theopathic.js

--- theopathic.js
+++ theopathic.js
@@ -1,2 +1,2 @@
-var sidebar = document.getElementById("sidebar");
-sidebar.innerHTML = "<font size=-2><a href=\"https://www.freepik.com/free-photo/sky_3277813.htm#query=sky&position=11&from_view=keyword&track=sph\">Image by rawpixel.com</a> on Freepik</font>" + sidebar.innerHTML;
+var aside = document.getElementByTagName("aside")[0];
+aside.innerHTML = "<font size=-2><a href=\"https://www.freepik.com/free-photo/sky_3277813.htm#query=sky&position=11&from_view=keyword&track=sph\">Image by rawpixel.com</a> on Freepik</font>" + aside.innerHTML;
