046f2476ab58de8bf63283ec7519bedb3bcb1b0d
theopathic <73636682+theopathic@users.noreply.github.com>
2023-11-03 20:40:21 -0600

    Create theopathic.js

---
+++ theopathic.js
@@ -0,0 +1,2 @@
+var sidebar = document.getElementById("sidebar");
+sidebar.innerHTML = "<font size=-2><a href=\"https://www.freepik.com/free-photo/sky_3277813.htm#query=sky&position=11&from_view=keyword&track=sph\">Image by rawpixel.com</a> on Freepik</font>" + sidebar.innerHTML;
